55898c4a9b403e5e2c475a76e53f8127147c4bf9
Andrew Bruss <apb61@case.edu>
2013-04-27 16:53:37 -0400

    start to Learner class

---
+++ Assignment5/Learner.java
@@ -0,0 +1,46 @@
+package Assignment5;
+
+public class ProbabilityMap{
+	
+	public static double[] features(StateView s, int fId, int eId){
+		double[] featureArray = new double[6];
+		
+		//# of friendlies attacking e
+    		featureArray[0]=0;
+		
+		//health of friendly f
+    		featureArray[1]=0;
+
+    		//health of enemy e
+    		featureArray[2]=0;
+
+    		//is  e the enemy f is attacking
+    		featureArray[3]=0;
+
+    		//is e the closest to f
+    		featureArray[4]=0;
+
+    		//is f the closet to e
+    		featureArray[5]=0;
+		
+		return featureArray;
+	}
+
+	public static double reward(StateView s, int fId, int eId){
+		return 0.0;
+	}
+
+	public static double Q(StateView s, int fId, int eId, double[] weights, double[] ogWeights){
+		//Q(s, a) = w * f(s, a) +w0
+		double q = dotProduct(features(s, fId, eId), weights) + ogWeights;
+		return 0.0;
+	}
+	public static double dotProduct(double[] a, double[] b){
+		if( a.size != b.size)
+			return 0;
+		int prod = 0;
+		for(int i = 0; i<a.size; i++){
+			prod += a[i]*b[i];
+		return prod;
+	}
+}
