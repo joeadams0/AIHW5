82ce691ffa42147e75b37d03c22ebd5638203a19
Andrew Bruss <apb61@case.edu>
2013-04-30 12:29:13 -0400

    minor changes to Reward()" "

--- Assignment5/Learner.java
+++ Assignment5/Learner.java
@@ -26,14 +26,21 @@ public class ProbabilityMap{
 		return featureArray;
 	}
 
-	public static double reward(StateView s, int fId, int eId){
-		return 0.0;
+	public double friendlysAttackingE(StateView s, int fId, int eId){}
+	public double healthOfF(StateView s, int fId, int eId){{
+	public double HealthOfE(StateView s, int fId, int eId){}
+	public double fAttackingE(StateView s, int fId, int eId){}
+	public double EclosestEnemy(StateView s, int fId, int eId){}
+	public double FclosestFriendly(StateView s, int fId, int eId){}
+	
+	public static double reward(int enemiesKilled, int friendliesKilled, int friendlyDamage, int enemyDamage, int moves_required){
+		return enemiesKilled*100 - friendliesKilled*100 + enemyDamage - friendlyDamage - ((double)moves_required)*.01;
 	}
 
 	public static double Q(StateView s, int fId, int eId, double[] weights, double[] ogWeights){
 		//Q(s, a) = w * f(s, a) +w0
 		double q = dotProduct(features(s, fId, eId), weights) + ogWeights;
-		return 0.0;
+		return q;
 	}
 	public static double dotProduct(double[] a, double[] b){
 		if( a.size != b.size)
